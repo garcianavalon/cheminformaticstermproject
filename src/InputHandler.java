70352afcb5606ea7aada8a6ea76bb92923dd78bc
Enrique Garcia <garcianavalon@gmail.com>
2013-12-11 21:26:43 +0800

    Fixed error with Input class inside Main class

---
+++ src/InputHandler.java
@@ -0,0 +1,12 @@
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+
+public class InputHandler {
+	public IAtomContainer[] readSDF(String filename) {
+		return null;
+	}
+
+	public IAtomContainer readSmiles(String smiles) {
+		return null;
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,6 @@
-import org.openscience.cdk.interfaces.IAtomContainer;
 
 public class Main {
 
-	public class Input {
-
-		public IAtomContainer[] readSDF(String filename) {
-			return null;
-		}
-
-		public IAtomContainer readSmiles(String smiles) {
-			return null;
-		}
-
-	}
+	
 
 }
