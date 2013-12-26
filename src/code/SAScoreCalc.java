533d79fd49d8fb0138ae135176f7dfab5c285eb0
Enrique Garcia <garcianavalon@gmail.com>
2013-12-26 16:39:01 +0800

    Base methods size penalty calculation
    Created base methods and test class. Implemented size penalty
    calculation.

--- src/code/SAScoreCalc.java
+++ src/code/SAScoreCalc.java
@@ -1,10 +1,37 @@
 package code;
 import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IMolecule;
 
 public class SAScoreCalc {
+	
+	private IMolecule molecule;
+	
+	public SAScoreCalc(IMolecule molecule) {
+		this.molecule = molecule;
+	}
 
 	public double calculateScore(IAtomContainer atomContainer) {
 		return 0;
 	}
+	
+	public double calcRingComplexityScore() {
+		return 0;
+	}
+	
+	public double calcStereoComplexityScore() {
+		return 0;
+	}
+	
+	public double calcMacroCyclePenalty() {
+		return 0;
+	}
+	
+	public double calcSizePenalty(){
+		
+		//natoms**1.005 - natoms
+		int natoms = molecule.getAtomCount();
+		return Math.pow(natoms, 1.005) - natoms;
+	}
+	
 
 }
---
+++ src/test/SAScore/TestCalculations.java
@@ -0,0 +1,21 @@
+package test.SAScore;
+
+import java.io.FileNotFoundException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCalculations {
+	
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+}
