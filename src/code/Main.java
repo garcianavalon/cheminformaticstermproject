b719d15fd5b83a3e841e4cad76dc3979bbb0d137
Jonay Trénous <jtrenous@gmail.com>
2013-12-27 21:32:54 +0800

    Added some tests

--- src/code/Main.java
+++ src/code/Main.java
@@ -3,6 +3,8 @@ package code;
 
 
 
+import java.util.AbstractMap.SimpleEntry;
+
 import org.openscience.cdk.DefaultChemObjectBuilder;
 import org.openscience.cdk.exception.InvalidSmilesException;
 import org.openscience.cdk.smiles.SmilesParser;
@@ -14,6 +16,10 @@ public class Main {
 		ECFFragmenter fragments = new ECFFragmenter();
 		fragments.generateFragments(parser.parseSmiles("OC(=O)C1=C(C=CC=C1)OC(=O)C"));
 		System.out.println( (fragments.getFragmentsAsKeyValuePairs()).size() );
+		int cnt = 0;
+		for (SimpleEntry<Integer, Integer> se : fragments.getFragmentsAsKeyValuePairs())
+			cnt += se.getValue();
+		System.out.println(cnt);
 	}
 	
 	 
--- src/test/ECFFragmenter/TestFragmenting.java
+++ src/test/ECFFragmenter/TestFragmenting.java
@@ -1,44 +1,89 @@
 package test.ECFFragmenter;
 
-
 import java.util.AbstractMap;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.openscience.cdk.smiles.SmilesGenerator;
 
 import code.ECFFragmenter;
 import code.InputHandler;
 
 public class TestFragmenting {
-	
-	String[] testSMILES = {"ccc"};
-	ArrayList<ArrayList<AbstractMap.SimpleEntry <String, Integer>>> expectedResults;
+
+	String[] testSMILES = { "ccc" };
+	ArrayList<ArrayList<AbstractMap.SimpleEntry<String, Integer>>> expectedResults;
 	ArrayList<ArrayList<AbstractMap.SimpleEntry<String, Integer>>> results;
-	
-	
+	ArrayList<ArrayList<String>> expectedResultSets;
+	ArrayList<ArrayList<String>> resultSets;
+
 	@Before
 	public void setUp() {
 		ECFFragmenter fragmenter = new ECFFragmenter();
-	    InputHandler handler = new InputHandler();
-	    this.results = new ArrayList<ArrayList<AbstractMap.SimpleEntry<String, Integer>>>();
-	    this.expectedResults = new ArrayList<ArrayList<AbstractMap.SimpleEntry<String, Integer>>>();
-		for (int i = 0; i<testSMILES.length ; i++) {
-			this.expectedResults.add(new  ArrayList<AbstractMap.SimpleEntry<String, Integer>>());
+		InputHandler handler = new InputHandler();
+		this.results = new ArrayList<ArrayList<AbstractMap.SimpleEntry<String, Integer>>>();
+		this.expectedResults = new ArrayList<ArrayList<AbstractMap.SimpleEntry<String, Integer>>>();
+		for (int i = 0; i < testSMILES.length; i++) {
+			this.expectedResults
+					.add(new ArrayList<AbstractMap.SimpleEntry<String, Integer>>());
 			fragmenter.generateFragments(handler.readSmiles(testSMILES[i]));
 			this.results.add(fragmenter.getFragmentsAsSMILES());
 		}
-		
-		this.expectedResults.get(0).add(new SimpleEntry<String, Integer>("C[Y]", 2));
-		this.expectedResults.get(0).add(new SimpleEntry<String, Integer>("[Y]C[Y]", 1));
-		this.expectedResults.get(0).add(new SimpleEntry<String, Integer>("CC[Y]", 2));
+
+		this.expectedResults.get(0).add(
+				new SimpleEntry<String, Integer>(canonicalize("C[Y]"), 2));
+		this.expectedResults.get(0).add(
+				new SimpleEntry<String, Integer>(canonicalize("[Y]C[Y]"), 1));
+		this.expectedResults.get(0).add(
+				new SimpleEntry<String, Integer>(canonicalize("CC[Y]"), 2));
+		expectedResultSets = getSet(expectedResults);
+		resultSets = getSet(results);
 	}
+
+	private ArrayList<ArrayList<String>> getSet(
+			ArrayList<ArrayList<SimpleEntry<String, Integer>>> setOfBags) {
+		ArrayList<ArrayList<String>> setOfSets = new ArrayList<ArrayList<String>>();
+		for (ArrayList<SimpleEntry<String, Integer>> bag : setOfBags) {
+			ArrayList<String> set = new ArrayList<String>();
+			setOfSets.add(set);
+			for (SimpleEntry<String, Integer> entry : bag) {
+				if (!set.contains(entry.getKey()))
+					set.add(entry.getKey());
+			}
+		}
+		return setOfSets;
+	}
+
 	@Test
-	public void testSmiles() {
-			for (ArrayList<AbstractMap.SimpleEntry<String, Integer>> res : results)
+	public void assertExpectedResultSetsSubsetOfResultSets() {
+		for (int i = 0; i < testSMILES.length; i++)
+			for (String s : this.expectedResultSets.get(i))
+				junitx.framework.ListAssert
+						.assertContains(resultSets.get(i), s);
+	}
+
+	@Test
+	public void assertResultSetsSubsetOfExpectedResultSets() {
+		for (int i = 0; i < testSMILES.length; i++)
+			for (String s : this.resultSets.get(i))
+				junitx.framework.ListAssert.assertContains(
+						expectedResultSets.get(i), s);
+	}
+
+	@Test
+	public void assertEquals() {
+		for (ArrayList<AbstractMap.SimpleEntry<String, Integer>> res : results)
 			junitx.framework.ListAssert.assertContains(expectedResults, res);
 	}
-		
+
+	private String canonicalize(String smiles) {
+		InputHandler handler = new InputHandler();
+		SmilesGenerator sg = new SmilesGenerator();
+		return sg.createSMILES(handler.readSmiles(smiles));
+
+	}
+
 }
 
