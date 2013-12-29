787e10c9d857f6cfbce3141d5c27c080a8338f45
Jonay Trénous <jtrenous@gmail.com>
2013-12-29 19:05:50 +0800

    Merge

--- src/code/ECFFragment.java
+++ src/code/ECFFragment.java
@@ -1,4 +1,47 @@
 package code;
 public class ECFFragment {
+	private int count;
+	private String key;
+	private Double score;
+	
+	
+	public ECFFragment(int count, String key) {
+		super();
+		this.count = count;
+		this.key = key;
+		this.score = null;
+	}
+	
+	public ECFFragment(int count, String key, Double score) {
+		super();
+		this.count = count;
+		this.key = key;
+		this.score = score;
+	}
 
+	public int getCount() {
+		return count;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
+	
 }
--- src/code/FragmentContributionsCalc.java
+++ src/code/FragmentContributionsCalc.java
@@ -1,18 +1,47 @@
 package code;
-import java.util.Map;
+import java.util.ArrayList;
+import org.openscience.cdk.interfaces.IAtomContainer;
 
 public class FragmentContributionsCalc {
 
-	public Map<Integer,Double> getFragmentContributions() {
-		return null;
+	private ECFFragmenter fragmenter;
+	
+	
+	public static void main(String[] args) {
+		
+		
 	}
 
-	private void readContribution() {
-
+	/**
+	 * 
+	 * @param molecule A Molecule to be fragmented
+	 * @return The generated fragments
+	 */
+	public ArrayList<ECFFragment> fragmentMolecule(IAtomContainer molecule){
+		fragmenter.generateFragments(molecule);
+		return fragmenter.getFragmentsAsSMILES();
+	}
+	
+	/**
+	 * Update the List of fragmentdata with the values in the parameter
+	 * @param fragments - An ArrayList of ECFfragments to be inserted in the list of ECFfragments
+	 */
+	public void updateFragmentFrequencyData(ArrayList<ECFFragment> fragments){
+		
+	}
+	
+	/**
+	 * Translate the previously calculated list of fragments and frequency into a list
+	 * of Fragments and Contributions
+	 */
+	public void calculateContributions() {
+		
+	}
+	
+	public double getStoredContributions(ArrayList<ECFFragment> fragments) {
+		return 0;
 	}
 
-	private void calculateContributions() {
-
-	}
+	
 
 }
