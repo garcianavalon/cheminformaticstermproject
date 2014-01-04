e86f25f7b89f464708700f7aa825c2a8e3acc8ba
Jonay Trénous <jtrenous@gmail.com>
2014-01-04 16:30:54 +0800

    merge

--- src/code/ECFFragment.java
+++ src/code/ECFFragment.java
@@ -1,52 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-package code;
-public class ECFFragment {
-	private int count;
-	private String key;
-	private Double score;
-	
-	
-	public ECFFragment(int count, String key) {
-		super();
-		this.count = count;
-		this.key = key;
-		this.score = null;
-	}
-	
-	public ECFFragment(int count, String key, Double score) {
-		super();
-		this.count = count;
-		this.key = key;
-		this.score = score;
-	}
 
-	public int getCount() {
-		return count;
-	}
 
-	public Double getScore() {
-		return score;
-	}
-
-	public void setScore(Double score) {
-		this.score = score;
-	}
-
-	public void setCount(Integer count) {
-		this.count = count;
-	}
-
-	public String getKey() {
-		return key;
-	}
-
-	public void setKey(String key) {
-		this.key = key;
-	}
-
-}
-=======
 package code;
 public class ECFFragment {
 	private int count;
@@ -94,53 +47,4 @@ public class ECFFragment {
 	
 	
 }
->>>>>>> 787e10c9d857f6cfbce3141d5c27c080a8338f45
-=======
-package code;
-public class ECFFragment {
-	private int count;
-	private String key;
-	private Double score;
-	
-	
-	public ECFFragment(int count, String key) {
-		super();
-		this.count = count;
-		this.key = key;
-		this.score = null;
-	}
-	
-	public ECFFragment(int count, String key, Double score) {
-		super();
-		this.count = count;
-		this.key = key;
-		this.score = score;
-	}
 
-	public int getCount() {
-		return count;
-	}
-
-	public Double getScore() {
-		return score;
-	}
-
-	public void setScore(Double score) {
-		this.score = score;
-	}
-
-	public void setCount(Integer count) {
-		this.count = count;
-	}
-
-	public String getKey() {
-		return key;
-	}
-
-	public void setKey(String key) {
-		this.key = key;
-	}
-	
-	
-}
->>>>>>> 787e10c9d857f6cfbce3141d5c27c080a8338f45
